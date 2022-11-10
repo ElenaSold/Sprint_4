96d0a1b63f3f8c0e424a75f6909bd308f1ae38ad
ElenaSold <72863677+ElenaSold@users.noreply.github.com>
2022-11-10 22:28:27 +0300

    Delete Sprint_4 directory

--- Sprint_4/locators/base_page_locators.py
+++
@@ -1,5 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class TestLocatorsBasePage:
-    # куки, кнопка "да все привыкли"
-    cookie_button = (By.XPATH, ".//button[contains(@class, 'App_CookieButton')]")
